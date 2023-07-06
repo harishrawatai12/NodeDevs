6d7c02703cdc13fede1da8ad018847c6180c15bb
Harish Rawat <harish.rawat@appinventiv.com>
2023-07-06 17:09:07 +0530

    test

---
+++ p.js
@@ -0,0 +1,36 @@
+function a() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('a')
+            resolve(1);
+        }, 10000);
+    })
+}
+
+function b() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('b')
+            resolve(1);
+        }, 5000);
+    })
+}
+
+function c() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('c')
+            resolve(1);
+        }, 1000);
+    })
+}
+
+function aa() {
+    return a();
+}
+
+(async () => {
+    await aa();
+    b();
+})()
+
